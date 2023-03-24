5b81ceeab70a14886b7d99c5c62d3addbf166dcc
abutahir <tahir@codeops.tech>
2023-03-24 12:48:47 +0530

    Step functions tutorials

---
+++ stepfunctions-serverless-framework/index.js
@@ -0,0 +1,12 @@
+module.exports.firstFunction = async (event) => {
+  console.log("Passing the event as it is to second state")
+  return event
+};
+
+module.exports.secondFunction = async (event) => {
+  event.secondFunctionData = "This data has entered second function"
+  return {
+    statusCode: 200,
+    body: JSON.stringify(event),
+  };
+};
